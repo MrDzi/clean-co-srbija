4e70e42e64021f05151d4f13be3013256807a6b3
Dejan Hadziomerovic <dejanhadziomerovic@gmail.com>
2015-04-21 22:03:47 +0200

    Zavrsena HTML struktura, zapocet CSS. Eksportovani svi aseti

--- index.php
+++ index.php
@@ -19,14 +19,271 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-      <footer>
-        <p>&copy; Company 2015</p>
-      </footer>
+        <!-- FIXED HEADER (appears on scroll) -->
+        <header class="cleanco-header-fixed">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-md-3">
+                        <a href="#"><img src="assets/img/cleanco-logo-small.png" /></a>
+                    </div>
+
+                    <div class="col-md-7">
+                        <ul class="cleanco-header-info clearfix">
+                            <li>
+                                <i class="fa fa-phone"></i>
+                                <p>+381 11 614 93 31</br><span>office@cleanco.rs</span></p>
+                            </li>
+                            <li>
+                                <i class="fa fa-map-marker"></i>
+                                <p>Klare Cetkin 1e/12</br><span>Beograd</span></p>
+                            </li>
+                            <li>
+                                <i class="fa fa-clock-o"></i>
+                                <p>Pon-pet: 09-18h, sub: 09-15h</br><span>Nedeljom ne radimo</span></p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-2">
+                        <ul class="cleanco-header-social">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>    
+            </div>
+        </header>
+        <!-- FIXED HEADER ENDS -->
+
+        <!-- BIG BKG IMAGE SECTION -->
+        <div class="cleanco-big-image-bkg">
+            
+        <div class="cleanco-big-image-text">
+
+            <a href="#"><img src="assets/img/cleanco-logo.png" /></a>
+
+            <p>Koristite svoje vreme pametno - održavanje prepustite profesionalcima.</p>
+
+            <p>Spremni smo da preuzmemo kompletno održavanje vašeg poslovnog i životnog prostora.</p>
+
+        </div>
+
+        </div>
+        <!-- BIG BKG IMAGE SECTION ENDS -->
+
+        <!-- CTA SECTION -->
+        <section class="cleanco-cta">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="cleanco-cta-info col-md-9">
+                        <p>Zakažite pranje Vašeg vozila na lokaciji koja Vama odgovara!</p>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a class="cleanco-cta-btn" href="#" role="button">Link</a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- CTA SECTION ENDS -->
+
+        <!--  M A I N   S E C T I O N  -->
+
+            <!-- SERVICES SECTION -->
+            <section class="cleanco-services">
+                <div class="container">
+                    
+                    <h1>Naše usluge</h1>
+
+                    <div class="row">
+                        
+                        <div class="col-md-4">
+                            <img src="assets/img/usluge-1.png" />
+
+                            <h3>Mobilna perionica</h2>
+
+                            <p>Da bismo izašli u susret potrebama naših klijenata, osposobili smo pokretnu perionicu kojoj nije potreban izvor napajanja. Brzo stižemo do Vas i efikasno izvršavamo usluge pranja. Posedujemo izuzetne najsavremenije mašine i iskusan tim, koji Vam u svakom trenutku mogu pružiti usluge čišćenja, pranja, pranja pod visokim pritiskom, kao i dubinsko čišćenje vozila i kućnog nameštaja.</p>
+                            <p>Mašine za uklanjanje prljavštine koriste vodenu paru čime se smanjuje upotreba štetnih hemijskih sredstava, steriliše površina automobila i postiže veća efikasnost i bolji rezultat pranja.</p>
+                            <p>Od konkurencije nas izdvaja fleksibilnost  i vrhunska tehnologija pranja. Takođe, u ponudi imamo i veliki izbor auto kozmetike.</p>
+
+                        </div>
+
+                        <div class="col-md-4">
+                            <img src="assets/img/usluge-2.png" />
+
+                            <h3>Facility Management</h2>
+
+                            <p>Odeljenje  _________ bavi se poslovima održavanja i upravljanja poslovnim objektima.</p>
+                            <p>Uvođenjem potrebnih  servisa za rad i funkcionisanje poslovnih objekata povećavamo kvalitet života i rada u njima. U zavisnosti od kategorije objekta, standarda gradnje, opremljenosti, lokacije i namene, ponudićemo Vam nivo, obim i vrstu usluge za svaki objekat pojedinačno. Nudimo usluge  čišćenja i održavanja prostorija, industrijskog čišćenja, domarskih usluga i popravki. Takodje, nudimo usluge hemijskog čišćenja podnih površina, mašinskih instalacija i opreme, kuhinja restorana, restoranske opreme, roštilja, ventilacija,  AC sistema i grejanje.</p>
+                            <p>Na ovaj način dugoročno investirate u svoj poslovni objekat, a mi ćemo brigom o njemu indirektno uticati na komercijalnu vrednost i značajno mu produžiti vek trajanja.</p>
+
+                        </div>
+
+                        <div class="col-md-4">
+                            <img src="assets/img/usluge-3.png" />
+
+                            <h3>Projektovanje, izgradnja i održavanje zelenih površina</h2>
+
+                            <p>Firma CleanCo je između ostalog specijalizovana i za pružanje kompletnih usluga iz oblasti pejzažne arhitekture i hortikulture.</p>
+                            <p>U ovoj oblasti pokrivamo sve faze projektovanja, izgradnje i održavanja zelenih površina.</p>
+                            <p>Posedujemo punu licencu za projektovanje i izvođenje radova na uređenju zelenih povšina i trudimo se da kvalitetnom i profesionalnom uslugom ispunimo sve zahteve svojih klijenata.</p>
+
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            <!-- SERVICES SECTION ENDS -->
+
+            <!-- CLIENTS SECTION -->
+            <section class="cleanco-clients">
+                <div class="container">
+
+                    <h2>Naši klijenti:</h2>
+
+                    <div class="row">
+                        
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/vip-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/piraeus-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/actavis-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/ericsson-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/bambi-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/agricole-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/sport-vision-logo.png" />
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="cleanco-clients-rectangle">
+                                <img src="assets/img/vb-leasing-logo.png" />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+            <!-- CLIENTS SECTION ENDS -->
+
+            <!-- TESTIMONIALS SECTION -->
+            <section class="cleanco-testimonials">
+                <div class="container">
+
+                    <h2>Rekli su o nama</h2>
+                    
+                    <div id="cleanco-testimonials-carousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#cleanco-testimonials-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#cleanco-testimonials-carousel" data-slide-to="1"></li>
+                            <li data-target="#cleanco-testimonials-carousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <p>“Maecenas et facilisis quam. Nullam molestie, tellus eu laoreet euismod, massa dui interdum eros, eget imperdiet lorem elit eu libero. Sed faucibus ante vel metus vestibulum dictum. Morbi nec convallis neque. Pellentesque et orci tempus nunc malesuada mattis. Sed tempus orci volutpat hendrerit commodo.”</p>
+                                <p>Ime i prezime</p>
+                            </div>
+                            <div class="item">
+                                <p>“Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu. Ut scelerisque hendrerit tellus.”</p>
+                                <p>Ime i prezime</p>
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#cleanco-testimonials-carousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#cleanco-testimonials-carousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+                </div>
+            </section>
+            <!-- TESTIMONIALS SECTION ENDS -->
+
+        <!--  M A I N   S E C T I O N  ENDS -->
+
+        <!-- GOOGLE MAP SECTION -->
+        <div class="cleanco-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11317.904700441662!2d20.417203049731324!3d44.83223518835949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a650b4310aecb%3A0xa917a3484eff14e4!2s1e+Klare+Cetkin%2C+Beograd!5e0!3m2!1sen!2srs!4v1429643284801" width="100%" height="400" frameborder="0" style="border:0"></iframe>
+        </div>
+        <!-- GOOGLE MAP SECTION ENDS -->
+
+        <!-- FOOTER -->
+        <footer>
+            <div class="container">
+                
+                <div class="cleanco-footer-black">
+
+                    <h3>O nama</h3>
+
+                    <p>Privredno društvo CleanCo d.o.o. osnovano je 2011. godine u Beogradu u cilju pružanja higijenskih i usluga održavanja kako u poslovnim tako i u privatnim objektima.</p>
+                    <p>Naša specijalnost je kompletno održavanje poslovnih prostora, domaćinstava, njihove okoline, kao i voznog parka kojim raspolažu, po sistemu „ključ u ruke“. U cilju daljeg izlaska u susret svojim klijentima, uveli smo i usluge mobilne perionice. CleanCo je tu da Vam obezbedi profesionalno obučen personal koji će svojim umećem i vrhunskom opremom, pravovrememno učiniti da Vaš radni prostor i dom zablistaju.</p>
+                    <p>Od samog osnivanja naša misija je pružanje vrhunskog kvaliteta usluga kroz fleksibilnost, dostupnost i profesionalizam. Da smo postavili nove standarde potvrđuje i lista naših zadovoljnih klijenata.</p>
+
+                    <ul class="cleanco-header-social">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="cleanco-footer-shadow"></div>
+
+                <div class="cleanco-footer-copyright">
+                    <p>Copyright &copy; 2015 Company Name. All rights reserved.</p>                    
+                </div>
+
+            </div>
+        </footer>
+        <!-- FOOTER ENDS -->
+
     </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="assets/js/vendor/bootstrap.min.js"></script>
-
         <script src="assets/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
