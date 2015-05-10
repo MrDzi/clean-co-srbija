2873ddaea25a8939866ac95ce2fee892bcb19b4a
Dejan Hadziomerovic <dejanhadziomerovic@gmail.com>
2015-05-10 22:52:50 +0200

    ispravke na Uslugama, HTML validacija.

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                 <div class="row">
                     
                     <div class="col-md-3 col-sm-3">
-                        <a href="#"><img src="assets/img/cleanco-logo-small.png" /></a>
+                        <a href="#"><img src="assets/img/cleanco-logo-small.png" alt="CleanCo logo" /></a>
                     </div>
 
                     <div class="col-md-7 col-sm-9">
@@ -75,7 +75,7 @@
                 
                 <div class="cleanco-big-image-text">
 
-                    <a href="#" class="cleanco-logo"><img src="assets/img/cleanco-logo.png" /></a>
+                    <a href="#" class="cleanco-logo"><img src="assets/img/cleanco-logo.png" alt="CleanCo logo" /></a>
 
                     <div class="cleanco-big-image-hero">
                         <h1>Koristite svoje vreme pametno - održavanje prepustite profesionalcima.</h1>
@@ -90,7 +90,7 @@
         <!-- BIG BKG IMAGE SECTION ENDS -->
 
         <!-- CTA SECTION -->
-        <section class="cleanco-cta">
+        <div class="cleanco-cta">
             <div class="container">
                 <div class="row">
                     
@@ -104,7 +104,7 @@
 
                 </div>
             </div>
-        </section>
+        </div>
 
         <div class="cleanco-cta-form">
             <div class="container">
@@ -129,8 +129,8 @@
                             </div>
                             <div class="form-group">
                                 <label for="location">Lokacija<span>*</span></label>
-                                <select class="form-control cleanco-scheduling-location" required>
-                                  <option disabled selected></option>
+                                <select class="form-control cleanco-scheduling-location" id="location" name="location" required>
+                                  <option value="" disabled selected>- izaberite -</option>
                                   <option value="0">BOP</option>
                                   <option value="1">Đorđa Stanojevića</option>
                                   <option value="2">Vaša lokacija</option>
@@ -144,27 +144,28 @@
                         <div class="col-sm-4 col-xs-12">
                             <div class="input-append date form-group" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
                                 <label for="date">Datum<span>*</span></label>
-                                <input id="date" class="form-control datepicker" readonly="true" required>
+                                <input id="date" class="form-control datepicker" readonly required>
                             </div>
                             <div class="form-group">
                                 <label for="time">Vreme<span>*</span></label>
                                 <select id="time" class="form-control" required>
-                                  <option>09:00</option>
-                                  <option>09:30</option>
-                                  <option>10:00</option>
-                                  <option>10:30</option>
-                                  <option>11:00</option>
-                                  <option>11:30</option>
-                                  <option>12:00</option>
-                                  <option>12:30</option>
-                                  <option>13:00</option>
-                                  <option>13:30</option>
-                                  <option>14:00</option>
-                                  <option>14:30</option>
-                                  <option>15:00</option>
-                                  <option>15:30</option>
-                                  <option>16:00</option>
-                                  <option>16:30</option>
+                                  <option value="" disabled selected>- izaberite -</option>
+                                  <option value="0">09:00</option>
+                                  <option value="1">09:30</option>
+                                  <option value="2">10:00</option>
+                                  <option value="3">10:30</option>
+                                  <option value="4">11:00</option>
+                                  <option value="5">11:30</option>
+                                  <option value="6">12:00</option>
+                                  <option value="7">12:30</option>
+                                  <option value="8">13:00</option>
+                                  <option value="9">13:30</option>
+                                  <option value="10">14:00</option>
+                                  <option value="11">14:30</option>
+                                  <option value="12">15:00</option>
+                                  <option value="13">15:30</option>
+                                  <option value="14">16:00</option>
+                                  <option value="15">16:30</option>
                                 </select>
                             </div>
                         </div>
@@ -191,41 +192,48 @@
                         
                         <div class="col-sm-4 col-xs-12">
                             <div class="cleanco-services-single-inner cleanco-services-single-inner-left">
-                                <img src="assets/img/usluge-1.jpg" class="img-responsive" />
+                                <img src="assets/img/usluge-1.jpg" class="img-responsive" alt="Mobilna perionica" />
 
-                                <h3>Mobilna perionica</h3>
+                                <div class="cleanco-services-single-inner-text">
+                                    <h3>Mobilna perionica</h3>
 
-                                <p>Da bismo izašli u susret potrebama naših klijenata, osposobili smo pokretnu perionicu kojoj nije potreban izvor napajanja. Brzo stižemo do Vas i efikasno izvršavamo usluge pranja. Posedujemo izuzetne najsavremenije mašine i iskusan tim, koji Vam u svakom trenutku mogu pružiti usluge čišćenja, pranja, pranja pod visokim pritiskom, kao i dubinsko čišćenje vozila i kućnog nameštaja.</p>
-                                <p>Mašine za uklanjanje prljavštine koriste vodenu paru čime se smanjuje upotreba štetnih hemijskih sredstava, steriliše površina automobila i postiže veća efikasnost i bolji rezultat pranja.</p>
-                                <p>Od konkurencije nas izdvaja fleksibilnost  i vrhunska tehnologija pranja. Takođe, u ponudi imamo i veliki izbor auto kozmetike.</p>
-                                <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                    <p>Da bismo izašli u susret potrebama naših klijenata, osposobili smo pokretnu perionicu kojoj nije potreban izvor napajanja. Brzo stižemo do Vas i efikasno izvršavamo usluge pranja. Posedujemo izuzetne najsavremenije mašine i iskusan tim, koji Vam u svakom trenutku mogu pružiti usluge čišćenja, pranja, pranja pod visokim pritiskom, kao i dubinsko čišćenje vozila i kućnog nameštaja.</p>
+                                    <p>Mašine za uklanjanje prljavštine koriste vodenu paru čime se smanjuje upotreba štetnih hemijskih sredstava, steriliše površina automobila i postiže veća efikasnost i bolji rezultat pranja.</p>
+                                    <p>Od konkurencije nas izdvaja fleksibilnost  i vrhunska tehnologija pranja. Takođe, u ponudi imamo i veliki izbor auto kozmetike.</p>
+
+                                    <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-sm-4 col-xs-12">
                             <div class="cleanco-services-single-inner cleanco-services-single-inner-middle">
-                                <img src="assets/img/usluge-2.jpg" class="img-responsive" />
+                                <img src="assets/img/usluge-2.jpg" class="img-responsive" alt="Upravljanje objektima" />
 
-                                <h3>Facility Management</h3>
+                                <div class="cleanco-services-single-inner-text">
+                                    <h3>Upravljanje objektima</h3>
 
-                                <p>Odeljenje  _________ bavi se poslovima održavanja i upravljanja poslovnim objektima.</p>
-                                <p>Uvođenjem potrebnih  servisa za rad i funkcionisanje poslovnih objekata povećavamo kvalitet života i rada u njima. U zavisnosti od kategorije objekta, standarda gradnje, opremljenosti, lokacije i namene, ponudićemo Vam nivo, obim i vrstu usluge za svaki objekat pojedinačno. Nudimo usluge  čišćenja i održavanja prostorija, industrijskog čišćenja, domarskih usluga i popravki. Takodje, nudimo usluge hemijskog čišćenja podnih površina, mašinskih instalacija i opreme, kuhinja restorana, restoranske opreme, roštilja, ventilacija,  AC sistema i grejanje.</p>
-                                <p>Na ovaj način dugoročno investirate u svoj poslovni objekat, a mi ćemo brigom o njemu indirektno uticati na komercijalnu vrednost i značajno mu produžiti vek trajanja.</p>
-                                <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                    <p>Uvođenjem potrebnih  servisa za rad i funkcionisanje poslovnih objekata povećavamo kvalitet života i rada u njima. U zavisnosti od kategorije objekta, standarda gradnje, opremljenosti, lokacije i namene, ponudićemo Vam nivo, obim i vrstu usluge za svaki objekat pojedinačno. Nudimo usluge  čišćenja i održavanja prostorija, industrijskog čišćenja, domarskih usluga i popravki. Takodje, nudimo usluge hemijskog čišćenja podnih površina, mašinskih instalacija i opreme, kuhinja restorana, restoranske opreme, roštilja, ventilacija,  AC sistema i grejanje.</p>
+                                    <p>Na ovaj način dugoročno investirate u svoj poslovni objekat, a mi ćemo brigom o njemu indirektno uticati na komercijalnu vrednost i značajno mu produžiti vek trajanja.</p>
+
+                                    <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-sm-4 col-xs-12">
                             <div class="cleanco-services-single-inner cleanco-services-single-inner-right">
-                                <img src="assets/img/usluge-3.jpg" class="img-responsive" />
+                                <img src="assets/img/usluge-3.jpg" class="img-responsive" alt="Upravljanje zelenim povrsinama" />
 
-                                <h3>Projektovanje, izgradnja i održavanje zelenih površina</h3>
+                                <div class="cleanco-services-single-inner-text">
+                                    <h3>Projektovanje, izgradnja i održavanje zelenih površina</h3>
 
-                                <p>Firma CleanCo je između ostalog specijalizovana i za pružanje kompletnih usluga iz oblasti pejzažne arhitekture i hortikulture.</p>
-                                <p>U ovoj oblasti pokrivamo sve faze projektovanja, izgradnje i održavanja zelenih površina.</p>
-                                <p>Posedujemo punu licencu za projektovanje i izvođenje radova na uređenju zelenih povšina i trudimo se da kvalitetnom i profesionalnom uslugom ispunimo sve zahteve svojih klijenata.</p>
-                                <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                    <p>Firma CleanCo je između ostalog specijalizovana i za pružanje kompletnih usluga iz oblasti pejzažne arhitekture i hortikulture.</p>
+                                    <p>U ovoj oblasti pokrivamo sve faze projektovanja, izgradnje i održavanja zelenih površina.</p>
+                                    <p>Posedujemo punu licencu za projektovanje i izvođenje radova na uređenju zelenih povšina i trudimo se da kvalitetnom i profesionalnom uslugom ispunimo sve zahteve svojih klijenata.</p>
 
+                                    <a class="cleanco-services-btn hidden-lg hidden-md hidden-sm" href="#" role="button"></a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -244,56 +252,56 @@
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/vip-logo.png" class="img-responsive" />
+                                    <img src="assets/img/vip-logo.png" class="img-responsive" alt="Vip" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/piraeus-logo.png" class="img-responsive" />
+                                    <img src="assets/img/piraeus-logo.png" class="img-responsive" alt="Piraeus" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/actavis-logo.png" class="img-responsive" />
+                                    <img src="assets/img/actavis-logo.png" class="img-responsive" alt="Actavis" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/ericsson-logo.png" class="img-responsive" />
+                                    <img src="assets/img/ericsson-logo.png" class="img-responsive" alt="Ericsson" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/bambi-logo.png" class="img-responsive" />
+                                    <img src="assets/img/bambi-logo.png" class="img-responsive" alt="Bambi" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/agricole-logo.png" class="img-responsive" />
+                                    <img src="assets/img/agricole-logo.png" class="img-responsive" alt="Agricole" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/sport-vision-logo.png" class="img-responsive" />
+                                    <img src="assets/img/sport-vision-logo.png" class="img-responsive" alt="Sport Vision" />
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-4 col-xs-12">
                             <div class="cleanco-clients-rectangle">
                                 <div class="cleanco-clients-rectangle-inner">
-                                    <img src="assets/img/vb-leasing-logo.png" class="img-responsive" />
+                                    <img src="assets/img/vb-leasing-logo.png" class="img-responsive" alt="VB Leasing" />
                                 </div>
                             </div>
                         </div>
@@ -403,7 +411,7 @@
         </footer>
         <!-- FOOTER ENDS -->
 
-    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
         <script src="assets/js/vendor/bootstrap-datepicker.js"></script>
